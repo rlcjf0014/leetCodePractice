9bb05a63c2d03cf7cbde221be834a40e43f6dc8b
Joshua Song <joshuarule2010@gmail.com>
2021-10-05 15:11:38 -0500

    Completed BestTimeToBuyStock

--- leetCodePractice/array/BestTimeToBuyStock.js
+++ leetCodePractice/array/BestTimeToBuyStock.js
@@ -3,9 +3,20 @@
  * @return {number}
  */
  var maxProfit = function(prices) {
-    let maxProfit = 0; 
+    /**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min = prices[0], max = 0;
+    
     for (let i =0; i < prices.length; i++) {
-        let secondI = 
+        if (prices[i] - min > max) max = prices[i] - min;
+          
+        if (prices[i] < min) min = prices[i];
     }
+    
+    return max;
+};
     
 };
