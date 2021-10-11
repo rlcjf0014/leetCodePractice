33e8fb951f1aa077a5fd013dc50f397c81efb1c9
Joshua Song <joshuarule2010@gmail.com>
2021-10-11 00:30:11 -0500

    Completed keys and rooms

---
+++ leetCodePractice/search/keyandroom.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+ var canVisitAllRooms = function(rooms) {
+    
+    const dfs = (currentRoom) => {
+        currentRoom.forEach((key) => {
+            if (!visited.has(key)) {
+                visited.add(key);
+                total += 1;
+                dfs(rooms[key])
+            }
+        })
+        
+    }
+    
+    let visited = new Set();
+    visited.add(0);
+    let total = 0;
+    
+    dfs(rooms[0]);
+    
+    return total === rooms.length - 1 ? true : false;
+};
