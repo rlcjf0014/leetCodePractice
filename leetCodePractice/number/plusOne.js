0f530504723131c444de05fba2ee059ff69209cc
Joshua Song <joshuarule2010@gmail.com>
2021-10-15 04:03:42 -0500

    Completed plus one

---
+++ leetCodePractice/number/plusOne.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+ var plusOne = function(digits) {
+    for(let i = digits.length - 1; i >= 0; i--){
+        if(digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    
+    let res = new Array(digits.length + 1).fill(0);
+    res[0] = 1;
+    return res;
+};
