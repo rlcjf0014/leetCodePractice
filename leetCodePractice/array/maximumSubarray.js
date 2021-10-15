02a110242e99032ba6438900c207a24350eed280
Joshua Song <joshuarule2010@gmail.com>
2021-10-15 00:39:53 -0500

    really finished maximum Subarray

--- leetCodePractice/array/maximumSubarray.js
+++ leetCodePractice/array/maximumSubarray.js
@@ -3,5 +3,13 @@
  * @return {number}
  */
  var maxSubArray = function(nums) {
+    let max = -Infinity;
+    let currentMax = 0;
     
+    for(let i = 0; i < nums.length; i++) {
+        currentMax = Math.max(nums[i], currentMax + nums[i]);
+        max = Math.max(currentMax, max);               
+    }
+    
+    return max;
 };
