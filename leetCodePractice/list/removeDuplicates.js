c23b6b2d72a4f61d17d9d113858b4492c7cb6ee2
Joshua Song <joshuarule2010@gmail.com>
2021-11-27 19:21:59 +0900

    completed remove duplicate in linked list

--- leetCodePractice/list/removeDuplicates.js
+++ leetCodePractice/list/removeDuplicates.js
@@ -10,5 +10,14 @@
  * @return {ListNode}
  */
  var deleteDuplicates = function(head) {
-    dddd
+    let curr = head;
+    while(curr&&curr.next) {
+        if (curr.val === curr.next.val) {
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;
+        }
+    
+    }
+    return head;
 };
