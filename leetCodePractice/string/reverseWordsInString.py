f8288366fcce166e4db2f18b0d899c46bc5c8c44
Joshua Song <joshuarule2010@gmail.com>
2022-01-18 22:36:33 -0600

    like my solution better

--- leetCodePractice/string/reverseWordsInString.py
+++ leetCodePractice/string/reverseWordsInString.py
@@ -4,11 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        # wordArr = s.strip().split(" ")
-        # result = [element.strip() for element in wordArr if len(element) != 0]
-        # result.reverse()
-        # return " ".join(result)
-        return " ".join(reversed(s.split()))
+        wordArr = s.strip().split(" ")
+        result = [element.strip() for element in wordArr if len(element) != 0]
+        result.reverse()
+        return " ".join(result)
+        # return " ".join(reversed(s.split())) 
        
         
         
