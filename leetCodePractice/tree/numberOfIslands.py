280af686c037d9ad39acb012a428590a26ae9c38
Joshua Song <joshuarule2010@gmail.com>
2022-01-20 17:40:25 -0600

    Added comments for numberOfIslands.py

--- leetCodePractice/tree/numberOfIslands.py
+++ leetCodePractice/tree/numberOfIslands.py
@@ -4,21 +4,26 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
+        # If nothing comes out, return 0
         if not grid:
             return 0
         
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
+                # Go through all the elements and check whether it's 1 or not
                 if grid[i][j] == '1':
                     self.dfs(grid, i, j)
                     count += 1
         return count
 
     def dfs(self, grid, i, j):
+        # If the length does not match or is not 1, return
         if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
             return
+        # If it is 1, change its mark
         grid[i][j] = '#'
+        # Check its adjacent element
         self.dfs(grid, i+1, j)
         self.dfs(grid, i-1, j)
         self.dfs(grid, i, j+1)
