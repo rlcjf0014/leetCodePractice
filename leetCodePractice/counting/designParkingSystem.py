5db18efbd1599b11e15999107cad19deff8aa0e8
Joshua Song <joshuarule2010@gmail.com>
2022-06-05 22:34:02 -0500

    completed design parking system in python

---
+++ leetCodePractice/counting/designParkingSystem.py
@@ -0,0 +1,21 @@
+class ParkingSystem(object):
+    __slots__ = ['_parking']
+	
+    def __init__(self, big, medium, small):
+        self._parking = {
+            1: big, 
+            2: medium, 
+            3: small
+        }
+
+    def addCar(self, carType):
+        available_spots = self._parking[carType]
+        if available_spots:
+            self._parking[carType] = available_spots - 1
+            return True
+        
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
