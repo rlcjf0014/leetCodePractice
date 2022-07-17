42af4fc38885fa8b563402efcfb95378578fa6a4
Joshua Song <joshuarule2010@gmail.com>
2022-07-16 20:11:00 -0500

    completed keep multiplying found values by two in python

---
+++ leetCodePractice/list/keepMultiplyingFoundValuesByTwo.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        """
+        :type nums: List[int]
+        :type original: int
+        :rtype: int
+        """
+        target = original
+        while target in nums:
+            target *= 2
+        return target
