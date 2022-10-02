7ae4d288279c590171ff58295b6ea832e304ebdb
Joshua Song <joshuarule2010@gmail.com>
2022-10-01 23:52:35 -0500

    completed valid palindrome in python

--- leetCodePractice/string/validPalindrome.py
+++ leetCodePractice/string/validPalindrome.py
@@ -16,3 +16,20 @@ class Solution:
                 letters+=letter.lower()
         
         return letters == letters[::-1]
+
+        i = 0
+        j = len(s) - 1
+
+        while i < j:
+            while i < len(s) and s[i].isalnum() == False:
+                i+=1
+            while j > -1 and s[j].isalnum() == False:
+                j-=1
+            if i >= len(s) or j <= -1:
+                break
+            if s[i].lower() != s[j].lower():
+                return False
+            i+=1
+            j-=1
+        
+        return True
