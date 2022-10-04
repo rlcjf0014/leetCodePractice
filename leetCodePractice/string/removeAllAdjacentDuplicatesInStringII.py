59a3c019c4b2e070e1cc19e634cdc9215b1d2eb3
Joshua Song <joshuarule2010@gmail.com>
2022-10-04 01:07:12 -0500

    completed remove all adjacent duplicates in String II in python

---
+++ leetCodePractice/string/removeAllAdjacentDuplicatesInStringII.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = [['#', 0]]
+        for c in s:
+            if stack[-1][0] == c:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([c, 1])
+        result = ""
+        for curr in stack:
+            tempStr = curr[0] * curr[1]
+            result += tempStr
+
+        return result
