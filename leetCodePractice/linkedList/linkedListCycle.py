e41de5e364ef22b597e8631623d317c24bbf6ab4
Joshua Song <joshuarule2010@gmail.com>
2022-10-09 00:56:10 -0500

    completed linked list cycle in python

---
+++ leetCodePractice/linkedList/linkedListCycle.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        record = {}
+
+        while head:
+            if head in record:
+                return True
+            record[head] = 0
+            head = head.next
+        return False
+        
+        # # return False
+        # nodes_seen = set()
+        # while head is not None:
+        #     if head in nodes_seen:
+        #         return True
+        #     nodes_seen.add(head)
+        #     head = head.next
+        # return False
