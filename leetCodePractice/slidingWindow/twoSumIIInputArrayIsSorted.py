2d444d7244ca0cf3ee26b7e0d211e8ca1e0ff681
Joshua Song <joshuarule2010@gmail.com>
2022-11-04 23:57:46 -0500

    completed two sum II also in sliding window

--- leetCodePractice/slidingWindow/twoSumIIInputArrayIsSorted.py
+++ leetCodePractice/slidingWindow/twoSumIIInputArrayIsSorted.py
@@ -1,11 +1,25 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        record = {}
-        for i in range(len(numbers)):
-            want = target - numbers[i]
-            if want in record:
-                return [record[want] + 1, i + 1]
+        # record = {}
+        # for i in range(len(numbers)):
+        #     want = target - numbers[i]
+        #     if want in record:
+        #         return [record[want] + 1, i + 1]
+        #     else:
+        #         record[numbers[i]] = i
+
+        l = 0
+        r = len(numbers) - 1
+
+        while l != r:
+            if (numbers[l] + numbers[r]) > target:
+                r -= 1
+            elif (numbers[l] + numbers[r]) < target:
+                l += 1
             else:
-                record[numbers[i]] = i
+                return [l+1, r+1]
+    
+
+        
 
         
