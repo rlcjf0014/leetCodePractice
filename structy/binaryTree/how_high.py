09c9f1ddf45ec0e6c899ad23b0df900f7a47a17a
Joshua Song <songj2@carleton.edu>
2023-04-30 21:49:52 -0500

    completed how high

---
+++ structy/binaryTree/how_high.py
@@ -0,0 +1,14 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.left = None
+#     self.right = None
+
+def how_high(node):
+  if not node:
+    return -1
+
+  left_height = how_high(node.left)
+  right_height = how_high (node.right)
+  
+  return 1 + max(left_height, right_height)
